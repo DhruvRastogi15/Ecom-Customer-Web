ecaf1800a4e1ce2b8e1d9398202d1c9f60ffacb1
Dhruv Rastogi` <dhruv.rastogi@protonshub.com>
2022-09-27 23:02:50 +0530

    TypeScript implementation and adding material UI

---
+++ ecomcustomerweb/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginSignup from "./Components/Login/LoginSignup";
+import './App.css'
+
+export default function App() {
+    return (
+        <div className="App">
+            <LoginSignup />
+            {/* <h1>React from Scratch</h1> */}
+        </div>
+    );
+}
---
+++ ecomcustomerweb/src/Components/Login/LoginSignup.tsx
@@ -0,0 +1,28 @@
+import React,{useState,useEffect} from "react";
+import Button from '@mui/material/Button';
+
+const LoginSignup = () => {
+
+    const [count, setCount] = useState(0)
+
+    useEffect(()=>{
+        alert('a')
+    },[])
+
+    function showAlert(): void {
+        setCount(count+1)
+        // throw new Error("Function not implemented.");
+    }
+
+    return(
+        // <h1>Hello Dhruv</h1>
+        <div>
+            <Button onClick={()=>showAlert()} variant="contained">Hello World</Button>
+            <h1>{count}</h1>
+        </div>
+    )
+}
+
+export default LoginSignup;
+
+
